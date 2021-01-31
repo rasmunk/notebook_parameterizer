86966f81250d958c4a74f3ecaf6c9dbee4a7b529
Rasmus Munk <munk1@live.dk>
2021-01-31 17:33:37 +0100

    Reformat code with black and update the README to give an example usage of the notebook_parameterizer

--- README.rst
+++ README.rst
@@ -7,5 +7,56 @@ notebook_parameterizer
 .. image:: https://badge.fury.io/py/notebook-parameterizer.svg
     :target: https://badge.fury.io/py/notebook-parameterizer
 
-A tool to generate parameterized Jupyter Notebooks.
+A tool to generate parameterized Jupyter Notebooks. This tool is similar to that of Papermill, but acts as a compliment
+to parameterize a Notebook not based on a preset cell that contains the parameters, but allows for parameterization
+throughout a Notebook without having to specify so.
+
+Instead the `notebook_parameterizer` uses a parameters YAML file to define which variables should be set to what value.
+With these preset variables in the parameters file, the `notebook_parameterizer` searches every cell for variable assignments 
+that match the given key in the parameters file.
+
+-----
+Usage
+-----
+
+An example of how to use the `notebook_parameterizer` can be seen below::
+
+    $ notebook_parameterizer -h
+    Usage: notebook_parameterizer [OPTIONS] NOTEBOOK_PATH PARAMETERS_PATH
+
+    Options:
+    -o, --output_notebook_path TEXT
+                                    Path to the parameterized output notebook
+    -e, --expand_env_values         Should ENV_ prefixed parameter values be
+                                    expanded to their matching OS environment
+                                    variable value
+
+    -h, --help                      Show this message and exit.
+
+
+The NOTEBOOK_PATH is the path to the Notebook that should be parameterized.
+
+The PARAMETERS_PATH is the path to the YAML file that contains the variables that should be parameterized.
+
+For instance, given the following Notebook in `example/notebooks/hello_world.ipynb`.
+
+.. image:: res/hello_world.png
+
+
+We can paramterize the Notebook with the `example/parameters/hello_world.yml` parameters file::
+
+    num: 1000
+    div_by: 4
+
+
+This is achived by executing the following command::
+
+    $ notebook_parameterizer example/notebooks/hello_world.ipynb \
+     example/parameters/hello_world.yml \
+     -o new_hello_world.ipynb
+
+
+After applying the `notebook_parameterizer`, the generated `new_hello_world.ipynb` Notebook has the following content.
+
+.. image:: res/parameterized_hello_world.png
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.dirname(__file__)
 
 
 def read(path):
-    with open(path, 'r') as _file:
+    with open(path, "r") as _file:
         return _file.read()
 
 
@@ -15,41 +15,41 @@ def read_req(name):
 
 
 version_ns = {}
-with open(os.path.join(here, 'version.py')) as f:
+with open(os.path.join(here, "version.py")) as f:
     exec(f.read(), {}, version_ns)
 
-long_description = open('README.rst').read()
+long_description = open("README.rst").read()
 setup(
-    name='notebook_parameterizer',
-    version=version_ns['__version__'],
-    description='A tool to generate parameterized Jupyter Notebooks',
-    author='Rasmus Munk',
-    author_email='rasmus.munk@nbi.ku.dk',
-    license='MIT',
-    keywords='jupyter notebook parameters',
+    name="notebook_parameterizer",
+    version=version_ns["__version__"],
+    description="A tool to generate parameterized Jupyter Notebooks",
+    author="Rasmus Munk",
+    author_email="rasmus.munk@nbi.ku.dk",
+    license="MIT",
+    keywords="jupyter notebook parameters",
     long_description=long_description,
-    url='https://github.com/rasmunk/notebook_parameterizer',
-    packages=['notebook_parameterizer'],
-    install_requires=read_req('requirements.txt'),
+    url="https://github.com/rasmunk/notebook_parameterizer",
+    packages=["notebook_parameterizer"],
+    install_requires=read_req("requirements.txt"),
     extras_require={
-        "test": read_req('requirements-dev.txt'),
-        "dev": read_req('requirements-dev.txt')
+        "test": read_req("requirements-dev.txt"),
+        "dev": read_req("requirements-dev.txt"),
     },
     entry_points={
-        'console_scripts': ['notebook_parameterizer = '
-                            'notebook_parameterizer.cli:notebook_parameterizer'
-                            ]},
-    project_urls={
-        'Source Code': 'https://github.com/rasmunk/notebook_parameterizer'
+        "console_scripts": [
+            "notebook_parameterizer = "
+            "notebook_parameterizer.cli:notebook_parameterizer"
+        ]
     },
+    project_urls={"Source Code": "https://github.com/rasmunk/notebook_parameterizer"},
     classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
-    ]
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
